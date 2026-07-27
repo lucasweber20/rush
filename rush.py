f8a31540b0418135e8658263ad5aa6128f41f882
lucasweber20 <lucasweber449@gmail.com>
2026-07-27 08:03:22 -0300

    Refactor: change arguments flags

--- rush.py
+++ rush.py
@@ -3,8 +3,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-args = parser.add_argument("-u", "--url", help='Set url, example: -u https://example.com/?param=value', type=str)
-args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
+args = parser.add_argument("-ip", "--ip", help='Set ip, example: -ip 192.168.0.100', type=str)
+args = parser.add_argument("-l", "--list", help="Specify list ip, example: -l ips.txt", type=str)
+args = parser.add_argument("-p", "--port", help="Specify port to scan, example: -p 22", type=str)
 args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
 args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 2", type=int)
 
