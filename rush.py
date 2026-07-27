fba2ae8363b45ccc60b65ca55fccb8016d0267a1
lucasweber20 <lucasweber449@gmail.com>
2026-07-27 10:42:14 -0300

    Remove arguments flag output

--- rush.py
+++ rush.py
@@ -8,7 +8,6 @@ parser = argparse.ArgumentParser()
 args = parser.add_argument("-ip", "--ip", help='Set ip, example: -ip 192.168.0.100', type=str)
 args = parser.add_argument("-l", "--list", help="Specify list ip, example: -l ips.txt", type=str)
 args = parser.add_argument("-p", "--port", help="Specify port to scan, example: -p 22", nargs="+", type=str)
-args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
 args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 2", type=int)
 
 args = parser.parse_args()
