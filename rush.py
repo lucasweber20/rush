8d8ec86c2a1142d986764325149f90d00f86d713
lucasweber20 <lucasweber449@gmail.com>
2026-07-31 09:47:45 -0300

    Feat: add requests

--- rush.py
+++ rush.py
@@ -1,19 +1,23 @@
-import socket
-import re
 import argparse
-from urllib.parse import urlsplit
+from scripts.Requests import Requests
 
 
 parser = argparse.ArgumentParser()
 
 args = parser.add_argument("-ip", "--ip", help='Set ip, example: -ip 192.168.0.100', type=str)
 args = parser.add_argument("-l", "--list", help="Specify list ip, example: -l ips.txt", type=str)
-args = parser.add_argument("-p", "--port", help="Specify port to scan, example: -p 22 or -p 1-100", nargs="+", type=str)
+args = parser.add_argument("-p", "--port", help="Specify port to scan, example: -p 22 or -p 1-100", type=str)
 
 args = parser.parse_args()
 
 def main():
-    pass
+    # Flags
+    host = args.ip
+    port = args.port
+
+    # Requests
+    req = Requests(host, port)
+    req.requests()
 
 if __name__ == "__main__":
     main()
--- scripts/Requests.py
+++ scripts/Requests.py
@@ -1,11 +1,21 @@
-'''def requests(ip, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(10)
-        result = s.connect_ex((ip, port))
-        if result == 0:
-            print(f"\033[92mOpen: {port}\033[00m")
-        s.close()
-        return result
-    except:
-        pass'''
+import socket
+import re
+
+
+class Requests:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def requests(self):
+        port = int(self.port)
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(10)
+            result = s.connect_ex((self.host, port))
+            if result == 0:
+                print(f"\033[92mOpen: {self.port}\033[00m")
+            s.close()
+            return result
+        except:
+            pass
