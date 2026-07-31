583f8ed9e3d4b36f0dc60e27e57c7711c915d2f2
lucasweber20 <lucasweber449@gmail.com>
2026-07-31 10:53:33 -0300

    Feat: add multithreading

--- rush.py
+++ rush.py
@@ -1,13 +1,15 @@
 import concurrent.futures
 import argparse
+import time
 from scripts.Requests import Requests
 
 
 parser = argparse.ArgumentParser()
 
 args = parser.add_argument("-ip", "--ip", help='Set ip, example: -ip 192.168.0.100', type=str)
+args = parser.add_argument("-p", "--port", help="Specify port to scan, example: -p 22, 1-100 or 80,443", nargs="+", type=str)
 args = parser.add_argument("-l", "--list", help="Specify list ip, example: -l ips.txt", type=str)
-args = parser.add_argument("-p", "--port", help="Specify port to scan, example: -p 22, 1-100 or 80,443", type=str)
+args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 5", default=1, type=int)
 
 args = parser.parse_args()
 
@@ -15,11 +17,16 @@ def main():
     # Flags
     host = args.ip
     port = args.port
-    list = args.list
-
+    thread = args.thread
+    
     # Requests
-    req = Requests(host, port)
-    req.requests()
+    req = Requests()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=thread) as executor:
+        futures = [executor.submit(req.requests, host, port) for p in port]
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result == 0:
+                print(f"\033[92mOpen: {port[0]}\033[00m")
 
 if __name__ == "__main__":
     main()
--- scripts/Requests.py
+++ scripts/Requests.py
@@ -2,19 +2,15 @@ import socket
 
 
 class Requests:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
+    def __init__(self):
+        pass
 
-    def requests(self):
-        port = int(self.port)
-        host = self.host
+    def requests(self, host, port):
+        port = int(port[0])
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(10)
             result = s.connect_ex((host, port))
-            if result == 0:
-                print(f"\033[92mOpen: {self.port}\033[00m")
             s.close()
             return result
         except:
