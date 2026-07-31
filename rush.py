344258aa2ed135a75da30ecb6d45a45f39b277f1
lucasweber20 <lucasweber449@gmail.com>
2026-07-31 14:30:17 -0300

    Refactor: print host

--- rush.py
+++ rush.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 
 args = parser.add_argument("-ip", "--ip", help='Set ip, example: -ip 192.168.0.100', type=str)
 args = parser.add_argument("-p", "--port", help="Specify port to scan, example: -p 22, 1-100 or 80,443", type=str)
+args = parser.add_argument("-l", "--list", help="Specify list ip, example: -l ips.txt", type=str)
 args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 5", default=1, type=int)
 
 args = parser.parse_args()
@@ -17,6 +18,7 @@ def main():
     host = args.ip
     port = args.port
     thread = args.thread
+    hosts = args.list
 
     # Parser
     ports = Parser(port)
@@ -26,6 +28,7 @@ def main():
     req = Requests()
     with concurrent.futures.ThreadPoolExecutor(max_workers=thread) as executor:
         futures = [executor.submit(req.requests, host, port) for port in ports_list]
+        print(f"===== \033[92m{host}\033[00m =====")
         for future in concurrent.futures.as_completed(futures):
             result = future.result()
             if result[0] == 0:
