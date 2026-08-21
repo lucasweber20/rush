04019d6abdbd10eaa284bcaf445d8e02a986e29f
lucasweber20 <lucasweber449@gmail.com>
2026-08-21 15:18:51 -0300

    Feat: scan multiples hosts

--- rush.py
+++ rush.py
@@ -1,12 +1,14 @@
 import concurrent.futures
 import argparse
+import itertools
 from scripts.Requests import Requests
 from scripts.Parser import Parser
 
 
 parser = argparse.ArgumentParser()
 
-args = parser.add_argument("-ip", "--ip", help='Specify ip, example: -ip 192.168.0.100', type=str)
+args = parser.add_argument("-ip", "--ip", help='Specify ip, example: -ip 192.168.0.100', nargs="+", type=str)
+args = parser.add_argument("-f", "--file", help='Specify file with hosts, example: -f hosts.txt', type=str)
 args = parser.add_argument("-p", "--port", help="Specify port to scan, example: -p 22, 1-100 or 80,443", type=str)
 args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 5", default=1, type=int)
 args = parser.add_argument("-si", help="Search for hosts, example: -si", action='store_true')
@@ -16,9 +18,13 @@ args = parser.parse_args()
 def main():
     # Flags
     host = args.ip
+    file = args.file
     port = args.port
     thread = args.thread
     scan_hosts = args.si
+
+    if file:
+        host = open(file, 'r').read().splitlines()
 
     # Search host
     if scan_hosts:
@@ -36,11 +42,11 @@ def main():
     # Requests
     req = Requests()
     with concurrent.futures.ThreadPoolExecutor(max_workers=thread) as executor:
-        futures = [executor.submit(req.requests, host, port) for port in ports_list]
-        print(f"===== \033[92m{host}\033[00m =====")
+        futures = [executor.submit(req.requests, *args) for args in itertools.product(host, ports_list)]
         for future in concurrent.futures.as_completed(futures):
             result = future.result()
             if result[0] == 0:
+                print(f"==== \033[92m{result[2]}\033[00m =====")
                 print(f"\033[92mOpen: {result[1]}\033[00m")
 
 if __name__ == "__main__":
